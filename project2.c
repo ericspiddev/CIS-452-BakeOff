763d87c6a6d3a08fc1178467a17a19476ec8acee
Eric Spidle <ericspidledev@gmail.com>
2023-04-03 20:54:15 -0700

    setup basic thread stuff

--- project2.c
+++ project2.c
@@ -5,9 +5,11 @@
 #include <string.h>
 
 #define MAX_STR_SIZE 256
+void* baker(void* args);
 int getUserBakers(char* str);
 int main()
 {
+    pthread_t thread;
     char userString[MAX_STR_SIZE];
     char* endPtr;
     int numBakers = -1;
@@ -17,7 +19,11 @@ int main()
         numBakers = getUserBakers(userString);
     }while(numBakers == -1);
 
-    printf("Num bakers is %d\n", numBakers);
+    for(int i =0; i <= numBakers; i++)
+    {
+        pthread_create(&thread, NULL, baker, (void*) &i);
+        pthread_detach(thread);
+    }
     return 0;
 }
 
@@ -36,5 +42,8 @@ int getUserBakers(char* str)
 
 void* baker(void* args)
 {
-
+    int threadID = 0;
+    memcpy(&threadID, (int*)args, sizeof(int));
+    printf("hello from thread %d\n", threadID);
+    pthread_exit((void*) 0);
 }
