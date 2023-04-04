f191ea5f4189b7f571b565c17ddc06d1b5148362
Eric Spidle <ericspidledev@gmail.com>
2023-04-03 20:44:14 -0700

    Initial function that prompts for number of bakers and error checks user input

--- project2.c
+++ project2.c
@@ -2,9 +2,39 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <sys/sem.h>
+#include <string.h>
 
+#define MAX_STR_SIZE 256
+int getUserBakers(char* str);
 int main()
 {
-    printf("Hello Project 2\n");
+    char userString[MAX_STR_SIZE];
+    char* endPtr;
+    int numBakers = -1;
+    printf("******* The Great Bake Off ***********\n");
+    do{
+        printf("Please enter the number of bakers for the bake off: ");
+        numBakers = getUserBakers(userString);
+    }while(numBakers == -1);
+
+    printf("Num bakers is %d\n", numBakers);
     return 0;
+}
+
+int getUserBakers(char* str)
+{
+    char* endPtr;
+    fgets(str, MAX_STR_SIZE, stdin);
+    str = strtok(str, "\n");
+    int bakers = strtoul(str, &endPtr, 10);
+    if(*endPtr == '\0' && bakers > 0)
+        return bakers;
+    else
+        printf("Please enter a valid decimal number for bakers\n");
+        return -1;
+}
+
+void* baker(void* args)
+{
+
 }
